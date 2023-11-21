87ce443da34f506b951e40b4e6021a7502afc5da
biahmed <biahmed@nisum.com>
2023-11-21 18:49:26 +0500

    api change

--- app/api/doctors/route.js
+++ app/api/doctors/route.js
@@ -1,6 +1,5 @@
 export async function GET() {
     const res = await fetch('https://655cabe925b76d9884fdbed3.mockapi.io/api/v1/medicine',)
     const data = await res.json()
-   
-    return Response.json({ data })
+    return Response.json({ data: "" });
   }
