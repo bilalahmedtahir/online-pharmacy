957598013f426b4ed355564c246cb44115cc32f3
biahmed <biahmed@nisum.com>
2023-11-21 17:44:59 +0500

    api integration

--- app/api/doctors/route.js
+++ app/api/doctors/route.js
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch('https://data.mongodb-api.com/...',)
+    const res = await fetch('https://17cf953e-8d11-41c0-9b61-f0313dff41ed.mock.pstmn.io/getMed',)
     const data = await res.json()
    
     return Response.json({ data })
